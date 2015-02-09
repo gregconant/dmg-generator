f4ca9c5f4ab54ae9a8b4c3c0238af9ff0eb6c45e
Greg Conant <greg.conant@gmail.com>
2015-02-08 21:58:59 -0500

    gc - moving routing out of app JS and into a proper routes folder to call controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var controller = require('../app_server/controllers/main');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function(app) {
+	app.get('/', controller.index);
+};
