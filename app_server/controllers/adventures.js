e59a517808c941a99fbd8785e783747706bff009
Greg Conant <greg.conant@gmail.com>
2015-04-03 10:46:19 -0400

    gc - moving location-based adventure data out of view and into controller

--- app_server/controllers/adventures.js
+++ app_server/controllers/adventures.js
@@ -1,4 +1,46 @@
 /* GET adventures page. */
 exports.adventures = function(req, res) {
-  res.render('adventures', { header: 'Adventure Generator' });
+  res.render('adventures', { 
+	header: 'Adventure Generator',
+	adventureTypes: [ "Location-Based", "Event-Based"],
+	adventureInfo: [
+		{
+			adventureType: "Location-Based",
+			adventureComponents: [
+				{
+					title: "Location Type",
+					value: "Wilderness"
+				},
+				{
+					title: "Location Details",
+					value: "Escort an NPC to a destination"
+				},
+				{
+					title: "Other Goals",
+					value: "Interfere with the operation of a business"
+				},
+				{
+					title: "Adventure Villains",
+					value: "Giant bent on plunder"
+				},
+				{
+					title: "Adventure Allies",
+					value: "Sage"
+				},
+				{
+					title: "Adventure Patrons",
+					value: "Deity or celestial"
+				},
+				{
+					title: "Adventure Introduction",
+					value: "A town or village needs volunteers to go to the adventure location"
+				},
+				{
+					title: "Adventure Climax",
+					value: "The adventurers must discover the main villain's secret weakness before they can hope to defeat that villain."
+				}
+		]
+	}
+	]
+  });
 };
